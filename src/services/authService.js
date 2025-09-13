f562c6214d1c21957195dcf1cb440c8daf53f09f
2300030871 <143414609+2300030871@users.noreply.github.com>
2025-09-13 10:00:16 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "ec2-34-224-31-77.compute-1.amazonaws.com";
+const API_URL ="http://ec2-34-224-31-77.compute-1.amazonaws.com/auth"
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
